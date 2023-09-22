ce90c68b8218cd36820efa41ddbedc0b0f503002
Stéphane <99326895+stephane-boulanger@users.noreply.github.com>
2023-09-22 11:57:20 +0200

    Update openai.js
    API.env

--- src/services/openai.js
+++ src/services/openai.js
@@ -6,7 +6,7 @@ const openai = axios.create({
   baseURL: "https://api.openai.com/v1/engines/gpt-3.5-turbo-16k/completions", // Mettez à jour la base URL avec gpt-3.5-turbo
   headers: {
     "Content-Type": "application/json",
-    Authorization: `Bearer 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`,
+    Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
   },
 });
 
